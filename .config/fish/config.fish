96e1ec8a61134b40dc6aa3f65268265e18c4e5a9
Aprameyan <aprameyan77@gmail.com>
2026-01-17 20:24:36 +0530

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,19 +1,9 @@
 if status is-interactive # Commands to run in interactive sessions can go here
-
     # No greeting
     set fish_greeting
-
     # Use starship
     starship init fish | source
-    if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
-        cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
     end
-
     # Aliases
-    alias pamcan pacman
     alias ls 'eza --icons'
-    alias clear "printf '\033[2J\033[3J\033[1;1H'"
-    alias q 'qs -c ii'
-    alias codiumw 'codium --ozone-platform=wayland'
-    alias activatevenv 'source /home/aprameyan/code/python/.college/bin/activate.fish'
 end
